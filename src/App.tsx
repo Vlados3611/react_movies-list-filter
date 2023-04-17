3fdd3b0626c939f5a34a9d41e185690dcbd225f5
Vladyslav Penkin <vy891078@gmail.com>
2023-04-17 22:43:00 +0300

    added solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [searchMovie, setMovieToState] = useState('');
+
+  const setValueToMoveList = (event: any) => {
+    const { value } = event.target;
+
+    setMovieToState(value);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +28,16 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={setValueToMoveList}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList
+          movies={moviesFromServer}
+          searchMovie={searchMovie}
+        />
       </div>
 
       <div className="sidebar">
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 
 interface Props {
   movies: Movie[];
+  searchMovie: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList: React.FC<Props> = ({ movies, searchMovie }) => {
+  const filteredMovieList = movies.filter(
+    (movie) => (
+      movie.title.toLowerCase().includes(searchMovie.toLowerCase())
+    ),
+  );
+
+  return (
+    <div className="movies">
+      {filteredMovieList.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
